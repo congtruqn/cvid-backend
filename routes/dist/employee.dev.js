ec923b293ae9480bfc8867f8f67981922d4e8820
tronghoangbk <hoang.nguyentrong197@gmail.com>
2022-07-10 14:35:45 +0700

    a

--- routes/dist/employee.dev.js
+++ routes/dist/employee.dev.js
@@ -97,7 +97,7 @@ router.post('/register', function (req, res) {
               position: position,
               password: password,
               type: 4,
-              status: 0
+              status: 1
             });
             User.createUser(newEmployee, function (err, companys) {
               if (err) throw err;
